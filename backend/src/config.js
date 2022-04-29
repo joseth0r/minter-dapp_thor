c31223cdec2128d8ffb706203633d21b8742a001
joseth0r <thor_business@protonmail.com>
2022-04-29 13:29:07 +0200

    y

--- backend/src/config.js
+++ backend/src/config.js
@@ -63,7 +63,7 @@ const TOKENS_PER_MINT = 5; // maximum number of NFTs a user can mint in a single
 const PUBLIC_MINT_START_DATE = "2022-05-02T14:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-05-01T14:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-04-28T14:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%, 1000 bps=10%
 const ROYALTY_ADDRESS = "0x1C7dA8110cC275a01e86947CA0F3F2076F98cE0d"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
