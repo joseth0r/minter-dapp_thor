36fdf5867fc71bc6bda27cb8a3a3654ac7b7fe9e
joseth0r <thor_business@protonmail.com>
2022-04-29 13:58:33 +0200

    eee test

--- backend/src/config.js
+++ backend/src/config.js
@@ -60,7 +60,7 @@ const MINT_PRICE = 0.003; // Minting price per NFT. Rinkeby = ETH, Polygon = MAT
 const TOKENS_PER_MINT = 5; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-04-30T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-04-29T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = "2022-04-28T14:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
