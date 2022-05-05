87f53a4fd815c7d33796787a571083d8c034d6c3
joseth0r <thor_business@protonmail.com>
2022-05-05 21:11:41 +0200

    test

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -18,7 +18,7 @@ window.addEventListener("DOMContentLoaded", async () => {
     window.web3 = new Web3(window.web3.currentProvider);
   }
   else if (!window.web3) {
-    window.location.assign("https://metamask.app.link/dapp/" + MINT_LINK)
+    window.web3.location.assign("https://metamask.app.link/dapp/" + MINT_LINK)
     return
 }
   
