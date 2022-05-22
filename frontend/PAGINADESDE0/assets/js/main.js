9e7374b5fd5fc975de176deadaac0b02c543a080
joseth0r <thor_business@protonmail.com>
2022-05-22 15:32:38 +0200

    test

--- frontend/PAGINADESDE0/assets/js/main.js
+++ frontend/PAGINADESDE0/assets/js/main.js
@@ -1,27 +1,17 @@
-/*==================== SHOW MENU ====================*/
-const showMenu = (toggleId, navId) => {
-  const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId);
+/*==================== SHOW MENU MOBILE ====================*/
+(function () {
+	$('.hamburger-menu').on('click', function() {
+		$('.bar').toggleClass('animate');
+    var mobileNav = $('.mobile-nav');
+    mobileNav.toggleClass('hide show');
+    var body = document.body;
+    body.classList.toggle("no-scroll");
+	})
+})();
 
-  // Validate that variables exist
-  if (toggle && nav) {
-    toggle.addEventListener("click", () => {
-      // We add the show-menu class to the div tag with the nav_menu class
-      nav.classList.toggle("show-menu");
-    });
-  }
-};
-showMenu("nav-toggle", "nav-menu");
 
-/*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll(".nav_link");
 
-function linkAction() {
-  const navMenu = document.getElementById("nav-menu");
-  // When we click on each nav_link, we remove the show-menu class
-  navMenu.classList.remove("show-menu");
-}
-navLink.forEach((n) => n.addEventListener("click", linkAction));
+
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll("section[id]");
